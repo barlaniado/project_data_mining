1c515e0e5826bad717b18f0e89a7e8a476bd0890
anatkira <78455947+anatkira@users.noreply.github.com>
2021-02-14 21:38:17 -0500

    Add files via upload

---
+++ project_conf.py
@@ -0,0 +1,7 @@
+#SECTORS = ["Technology", "Basic materials", "Healthcare", "Energy"]
+SECTORS = ["Basic materials"]
+COUNT = 100
+headers = {
+    'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+}
