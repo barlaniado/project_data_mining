34378f19d998bef6c4e85a7d1a270f624eafedad
barlaniado <78263226+barlaniado@users.noreply.github.com>
2021-02-16 14:36:24 +0200

    Without constants and magic numbers

--- get_data_of_financial_statements.py
+++ get_data_of_financial_statements.py
@@ -12,14 +12,12 @@ def get_data_financial_statements(symbol, counter_symbols):
     data_dict = {}
     data_indicator = 0
     utilities.program_sleep(counter_symbols)
-    print(symbol)
+    print(symbol) # MAKE THIS PRING LOG
     soup = requests_webpage.get_content_financial_statements(symbol)
-    all_span = soup.find_all("span")
-    if len(all_span) < 100:
-        print("less then",len(all_span))
+    all_span = soup.find_all(project_conf.TAG_DATA_FINANCIAL_STATEMENTS)
     for i in all_span:
         current_text = i.text
-        if current_text == "Total Revenue":
+        if current_text == project_conf.TOTAL_REVENUE_TITLE:
             now_titles = 0
         if now_titles == 1:
             current_title = current_text
@@ -27,19 +25,20 @@ def get_data_financial_statements(symbol, counter_symbols):
             data_dict[current_title] = {}
         elif now_net_income == 1:
             try:
-                data_dict[title_list[counter]]["Net Income"] = int(current_text.replace(",", ""))
+                data_dict[title_list[counter]][project_conf.KEY_NET_INCOME] =\
+                int(current_text.replace(project_conf.DELETE_FROM_NET_INCOME_STRING, project_conf.REPLACE_DELETED_CHAR_WITH))
             except ValueError:
-                data_dict[title_list[counter]]["Net Income"] = None
+                data_dict[title_list[counter]][project_conf.KEY_NET_INCOME] = project_conf.VALUE_IF_CANT_CAST_TO_INT
             counter += 1
             if counter == len(title_list):
                 break
-        if current_text == "Breakdown":
+        if current_text == project_conf.NEXT_TO_COME_TITLES:
             now_titles = 1
-        if current_text == "Net Income Common Stockholders":
+        if current_text == project_conf.NEXT_TO_COME_DATA_NET_INCOME:
             data_indicator = 1
             now_net_income = 1
             counter = 0
-    if data_indicator ==1:
+    if data_indicator == 1:
         return {symbol: data_dict}
     else:
         return {symbol: None}
