ece321a13c2be01a32ef68c7ae7498aaf9d549ce
barlaniado <78263226+barlaniado@users.noreply.github.com>
2021-02-17 17:27:26 +0200

    Update requests_webpages.py

--- requests_webpages.py
+++ requests_webpages.py
@@ -2,16 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import project_conf
 import utilities
+import logging
+
+logger = logging.getLogger('data_mining')
 
 
 def get_content_sector_page(page_url):
     page = requests.get(page_url, headers=project_conf.HEADERS)
+    logger.info("The code of the current request: " + str(page.status_code))
     utilities.program_sleep()
     page_soup = BeautifulSoup(page.text, project_conf.HTML_PARSER)
+    logger.info("Return a BeautifulSoup object")
     return page_soup
 
 def get_content_financial_statements(symbol):
+    logger.info('extract data from the financial statements of: ' + symbol)
     current_url= utilities.build_url_financials_symbol(symbol)
+    logger.info('Sending a request to: ' + current_url)
     page = requests.get(current_url, headers=project_conf.HEADERS)
+    logger.info("The code of the current request: " + str(page.status_code))
     soup = BeautifulSoup(page.text,  project_conf.HTML_PARSER)
+    logger.info("Return a BeautifulSoup object")
     return soup
