79aba985aaa6426ea7eebd516fd8e2821b12ccc8
bar <you@example.com>
2021-05-22 10:16:18 +0300

    fix bugs

--- getData/main_scraper.py
+++ getData/main_scraper.py
@@ -171,7 +171,6 @@ class MainScraperSymbols:
     def _insert_recommendations(self):
         """ Insert the the recommendation to the db """
         for current_symbol_reco, reco_object in self.all_recommendations.dict_recommendations.items():
-                date_to_insert = reco_object.date_recommendation
                 if reco_object.data:
                     for type, how_many in reco_object.data.items():
                         query = project_conf.INSERT_RECOMMENDATION.format(project_conf.RECOMMENDATIONS_TABLE,
